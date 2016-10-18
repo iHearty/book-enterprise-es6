312a697a4e85afd880b26fcdd31c72b4cfaf6e32
古木风 <yanghailong@togeek.cn>
2016-10-18 17:45:58 +0800

    react-es6 project init

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import './main.scss';
+
+import * as React from 'react';
+
+export class App extends React.Component {
+   render() {
+      return (
+         <div className="main-wrapper">
+            <header></header>
+            <mian></mian>
+            <footer></footer>
+         </div>
+      );
+   }
+}
